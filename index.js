3b367703481d62b0803b86342858e6a17de69d71
Md. Jakir Hasan Rony <jakir15-14584@diu.edu.bd>
2023-12-04 00:20:49 +0600

    admin and default user functionalites plus jwt veryfication method added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -37,6 +38,126 @@ async function run() {
         const guidesCollection = client.db("finalEffortDb").collection("guides");
         const tourPackagesCollection = client.db("finalEffortDb").collection("tourPackages");
         const wishListCollection = client.db("finalEffortDb").collection("wishList");
+        const userCollection = client.db("finalEffortDb").collection("users");
+
+
+
+
+        // jwt related api
+
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token });
+        })
+
+
+
+        // middlewares
+        const verifyToken = (req, res, next) => {
+            console.log('inside verify token', req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'unauthorized access' });
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'unauthorized access' })
+                }
+                req.decoded = decoded;
+                next();
+
+            })
+        }
+
+        // verify admin
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                return req.status(403).send({ message: 'forbidden access' });
+            }
+            next();
+        }
+
+
+
+
+
+
+
+
+
+        // user related api
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exists', insertedId: null })
+            }
+
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+
+
+        app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
+
+
+        // admin api
+        app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+
+        })
+
+        // check if user admin or not!
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return req.status(403).send({ message: 'forbidden access' });
+            }
+
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'admin'
+            }
+            res.send({ admin });
+
+        })
+
+
+
+
+        // tour guide api
+        app.patch('/users/tourGuide/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    role: 'tourGuide'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+
+        })
 
 
 
@@ -67,13 +188,26 @@ async function run() {
             const result = await wishListCollection.insertOne(wishListItem);
             res.send(result);
         })
-        
-        app.get('/wishlist', async(req, res)=>{
+
+        app.get('/wishlist', async (req, res) => {
             const email = req.query.email;
             const query = { email: email };
             const result = await wishListCollection.find(query).toArray();
             res.send(result);
         })
+
+        app.delete('/wishlist/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log(id);
+            const query = { _id: new ObjectId(id) }
+            const result = await wishListCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+
+
+
 
 
 
